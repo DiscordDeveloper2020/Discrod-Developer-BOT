5406ab35631fac59cd76099fa9cf6fdd05a7f3a1
DiscordDeveloper2020 <69076499+DiscordDeveloper2020@users.noreply.github.com>
2020-08-01 15:42:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN)
+client.login("NzM5MDU5NDgzMzI5ODIyNzMw.XyU8xw.uG4LNrDAqziF0EZnY3TT3v3h80E")
 
 var prefix = ("d/")
 
@@ -13,6 +13,17 @@ client.on("message",(message) => {
 
 if(message.content === prefix + "help"){
     message.channel.send("**Le `d/help` n'est pas disponible pour le moment, il est encore en dévelopement !**")
+
+    if(message.content === prefix + "botinfo"){
+     var embed = new Discord.RichEmbed()
+     .setTitle("Infos BOT")
+     .setDescription("Ceci est une desvription")
+     .addField(".help","Page d'aide", true)
+     .addField("Embed01", "Embed 01 !", true)
+     ;setColor("0x23272A")
+     .setFooter("Ceci est un footer")
+     message.channel.sendEmbed(embed); 
+     
 }
 
 
